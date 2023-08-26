11baea6555e6e9692f7c35c7d3138e71e4a0355e
Stephen <steve.a.cheney99@gmail.com>
2023-08-26 16:04:44 -0400

    Refactor

--- src/biopytk/dnaToolkit.py
+++ src/biopytk/dnaToolkit.py
@@ -278,6 +278,28 @@ def getMaxGCFromFASTA(fasta_File):
     return {maxKey:gcDict[maxKey]}
 
 
+def getProteinsFromRF(amino_acid_seq):
+    """
+    Given an amino acid sequence, return all possible proteins
+    \n<- amino_acid_seq: chr[]
+    \n-> str[]
+    """    
+    curr_protein = ""
+    proteins = []
+
+    for each in amino_acid_seq:
+        if each == "_":
+            if bool(curr_protein):
+                proteins.append(curr_protein)
+                curr_protein = ""
+        else:
+            if each == "M":
+                curr_protein += each
+            if bool(curr_protein):
+                curr_protein += each
+
+    return proteins
+
 # ====== Function Comment Template ======
 
     """
